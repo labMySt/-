4c7dc88ea5da97fb23481bc0e744abaa5a7868bc
labMySt <33463536+labMySt@users.noreply.github.com>
2017-12-03 23:08:39 +0200

    Add files via upload

---
+++ navi/navi2.php
@@ -0,0 +1,31 @@
+<?
+
+if(!$_COOKIE["history"])
+
+	{
+		$cookie = array("http://myshop/navi/navi.php");
+    	setcookie("history", serialize($cookie), time()+3600);	 
+	}
+
+else 
+	{   
+		$cookie = unserialize($_COOKIE["history"]);
+		array_push($cookie, "http://myshop/navi/navi2.php");
+    	setcookie("history", serialize($cookie), time()+3600);	 
+	}
+echo "hello! This is my 2th page!";
+
+echo "<br>";
+?>
+
+<html>
+<a href="http://myshop/navi/navi.php">Go to navi</a>
+<a href="http://myshop/navi/navi2.php">Go to navi2</a>
+<a href="http://myshop/navi/navi3.php">go to navi3</a>
+
+</html>
+<?
+	echo "<pre>";
+    echo (var_dump(unserialize($_COOKIE["history"])));
+
+?>
